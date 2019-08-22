6d99cc5480d23505668284be3d9b1d5f937da146
CorentinAulagnier <corentinaulagnier@hotmail.fr>
2019-08-22 11:27:34 +0200

    fix test

--- test/test.sh
+++ test/test.sh
@@ -7,6 +7,24 @@ TEST_NAMESPACE=cert-manager-test
 COMMON_NAME=COMMON_NAME_DEFAULT
 
 #####
+# Removing secrets, certificates and issuers
+#####
+kubectl delete secret ca-keys-secret --namespace=${TEST_NAMESPACE}
+kubectl delete secret ca-keys-secret --namespace=cert-manager
+
+kubectl delete -f issuer-selfsigned --namespace=${TEST_NAMESPACE}
+kubectl delete -f issuer-ca --namespace=${TEST_NAMESPACE}
+kubectl delete -f clusterissuer-ca
+
+kubectl delete secret certificate-selfsigned-secret --namespace=${TEST_NAMESPACE}
+kubectl delete secret certificate-cluster-ca-secret --namespace=${TEST_NAMESPACE}
+kubectl delete secret certificate-ca-secret --namespace=${TEST_NAMESPACE}
+
+echo
+echo Self signed issuer test
+echo
+
+#####
 # Self signed issuer test
 #####
 
@@ -19,15 +37,19 @@ kubectl create -f certificate-selfsigned.yaml --namespace=${TEST_NAMESPACE}
 # Verification
 kubectl get secret certificate-selfsigned-secret --namespace=${TEST_NAMESPACE}
 
+echo
+echo Classic issuer test
+echo
+
 #####
 # Classic issuer test
 #####
 
 # Generate a CA private key
-$ openssl genrsa -out ca.key 2048
+openssl genrsa -out ca.key 2048
 
 # Create a self signed Certificate, valid for 10yrs with the 'signing' option set
-$ openssl req -x509 -new -nodes -key ca.key -subj "/CN=${COMMON_NAME}" -days 3650 -reqexts v3_req -extensions v3_ca -out ca.crt
+openssl req -x509 -new -nodes -key ca.key -subj "/CN=${COMMON_NAME}" -days 3650 -reqexts v3_req -extensions v3_ca -out ca.crt
 
 # Create a secret with the self signed Certificate, and the CA private key
 kubectl create secret tls ca-keys-secret --cert=ca.crt --key=ca.key --namespace=${TEST_NAMESPACE}
@@ -41,16 +63,19 @@ kubectl create -f certificate-ca.yaml --namespace=${TEST_NAMESPACE}
 # Verification
 kubectl get secret certificate-ca-secret --namespace=${TEST_NAMESPACE}
 
+echo
+echo Cluster issuer test
+echo
 
 #####
 # Cluster issuer test
 #####
 
 # Generate a cluster CA private key
-$ openssl genrsa -out ca.key 2048
+openssl genrsa -out ca.key 2048
 
 # Create a self signed Certificate, valid for 10yrs with the 'signing' option set
-$ openssl req -x509 -new -nodes -key ca.key -subj "/CN=${COMMON_NAME}" -days 3650 -reqexts v3_req -extensions v3_ca -out ca.crt
+openssl req -x509 -new -nodes -key ca.key -subj "/CN=${COMMON_NAME}" -days 3650 -reqexts v3_req -extensions v3_ca -out ca.crt
 
 # Create a secret with the self signed Certificate, and the CA private key
 # The secret must be in the cert-manager namespace
@@ -65,7 +90,9 @@ kubectl create -f certificate-cluster-ca.yaml --namespace=${TEST_NAMESPACE}
 # Verification
 kubectl get secret certificate-cluster-ca-secret --namespace=${TEST_NAMESPACE}
 
-
+echo
+echo End test
+echo
 
 # Removing CA key and cert file
 rm ca.key ca.crt
