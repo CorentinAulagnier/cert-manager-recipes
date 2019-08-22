fe9821b9e239fd0f655fcbef65199275f2bfd5a8
CorentinAulagnier <corentinaulagnier@hotmail.fr>
2019-08-22 15:14:54 +0200

    test v2

--- test/README.md
+++ test/README.md
@@ -1,8 +1,7 @@
 # cert-manager quick tests
 
 ## Tests
-
-### These tests are testing :
+These tests are testing :
 - selfsigned issuer creation
 - issuer creation
 - clusterissuer creation
@@ -11,17 +10,19 @@
 - certificate creation from issuer
 - certificate creation from clusterissuer
 
-### Comming soon :
 - certificate creation with selfsigned from Ingress
 - certificate creation with issuer from Ingress
 - certificate creation with clusterissuer from Ingress
 
 ## Parameters
-Two parameters can be modified in the bash file :
-- TEST_NAMESPACE: specify the namespace for tests
-- COMMON_NAME: the CN that are inside the certificates
-- SLEEP: waiting time between certificate and secret creation
+Many variables can be modified in the bash file :
+- TEST_NAMESPACE: specify the namespace for tests.
+- COMMON_NAME: the CN that are inside the certificates.
+- SLEEP: waiting time between certificate and secret creation.
 
+- CERT_FOLDER: the folder where certificates are stored.
+- ISSUER_FOLDER: the folder where issuer are stored.
+- INGRESS_FOLDER: the folder where ingress are stored.
 
 ## Start
 > sh test.sh
--- test/test.sh
+++ test/test.sh
@@ -9,6 +9,10 @@ COMMON_NAME=COMMON_NAME_DEFAULT
 
 SLEEP=2
 
+CERT_FOLDER="./certificate/"
+ISSUER_FOLDER="./issuer/"
+INGRESS_FOLDER="./ingress/"
+
 #####
 # Removing CA key and cert file, secrets, certificates and issuers
 #####
@@ -31,6 +35,15 @@ deleteRessources()
 	kubectl delete secret certificate-selfsigned-secret --namespace=${TEST_NAMESPACE} 	>/dev/null 2>&1
 	kubectl delete secret certificate-ca-secret --namespace=${TEST_NAMESPACE}		>/dev/null 2>&1
 	kubectl delete secret certificate-cluster-ca-secret --namespace=${TEST_NAMESPACE}	>/dev/null 2>&1
+
+	kubectl delete secret ingress-selfsigned-issuer-secret --namespace=${TEST_NAMESPACE}	>/dev/null 2>&1
+	kubectl delete secret ingress-issuer-secret --namespace=${TEST_NAMESPACE}		>/dev/null 2>&1
+	kubectl delete secret ingress-clusterissuer-secret --namespace=${TEST_NAMESPACE}	>/dev/null 2>&1
+
+	kubectl delete ingress ingress-selfsigned-issuer --namespace=${TEST_NAMESPACE}		>/dev/null 2>&1
+	kubectl delete ingress ingress-issuer --namespace=${TEST_NAMESPACE}			>/dev/null 2>&1
+	kubectl delete ingress ingressingress-clusterissuer --namespace=${TEST_NAMESPACE}	>/dev/null 2>&1
+
 }
 
 deleteRessources
@@ -45,13 +58,17 @@ echo Self signed issuer test
 echo
 
 # Create a self signed issuer
-kubectl create -f issuer-selfsigned.yaml --namespace=${TEST_NAMESPACE}
+kubectl create -f ${ISSUER_FOLDER}issuer-selfsigned.yaml --namespace=${TEST_NAMESPACE}
 
 # Create a certificate using the self signed issuer
-kubectl create -f certificate-selfsigned.yaml --namespace=${TEST_NAMESPACE}
+kubectl create -f ${CERTIFICATE_FOLDER}certificate-selfsigned.yaml --namespace=${TEST_NAMESPACE}
+
+# Create an ingress using certificate and the selfsigned issuer
+kubectl create -f ${INGRESS_FOLDER}ingress-selfsigned.yaml --namespace=${TEST_NAMESPACE}
 
 echo
 echo Verification
+echo certificate-selfsigned and ingress-selfsigned-issuer-secret secret must appear
 echo
 
 sleep ${SLEEP}
@@ -78,13 +95,17 @@ kubectl create secret tls ca-keys-secret --cert=ca.crt --key=ca.key --namespace=
 echo
 
 # Create an issuer using the previous secret
-kubectl create -f issuer-ca.yaml --namespace=${TEST_NAMESPACE}
+kubectl create -f ${ISSUER_FOLDER}issuer-ca.yaml --namespace=${TEST_NAMESPACE}
 
 # Create a certificate using the classic issuer
-kubectl create -f certificate-ca.yaml --namespace=${TEST_NAMESPACE}
+kubectl create -f ${CERTIFICATE_FOLDER}certificate-ca.yaml --namespace=${TEST_NAMESPACE}
+
+# Create an ingress using certificate and the classic issuer
+kubectl create -f ${INGRESS_FOLDER}ingress-issuer.yaml --namespace=${TEST_NAMESPACE}
 
 echo
 echo Verification
+echo certificate-ca-secret and ingress-issuer-secret secret must appear
 echo
 
 sleep ${SLEEP}
@@ -111,13 +132,17 @@ kubectl create secret tls cluster-ca-keys-secret --cert=ca.crt --key=ca.key --na
 echo
 
 # Create a cluster issuer using the previous secret
-kubectl create -f clusterIssuer-ca.yaml
+kubectl create -f ${ISSUER_FOLDER}clusterIssuer-ca.yaml
 
-# Create a certificate using the classic issuer
-kubectl create -f certificate-cluster-ca.yaml --namespace=${TEST_NAMESPACE}
+# Create a certificate using the cluster issuer
+kubectl create -f ${CERTIFICATE_FOLDER}certificate-cluster-ca.yaml --namespace=${TEST_NAMESPACE}
+
+# Create an ingress using certificate and the cluster issuer
+kubectl create -f ${INGRESS_FOLDER}ingress-clusterissuer.yaml --namespace=${TEST_NAMESPACE}
 
 echo
 echo Verification
+echo certificate-cluster-ca-secret and ingress-clusterissuer-secret secret must appear
 echo
 
 sleep ${SLEEP}
@@ -125,6 +150,9 @@ kubectl get secret certificate-cluster-ca-secret --namespace=${TEST_NAMESPACE}
 echo
 
 deleteRessources
+
+
+
 
 
 
