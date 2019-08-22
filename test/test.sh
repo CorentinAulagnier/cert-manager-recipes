9960fa42b9763ab2be2535aeb7ab3556c0acc272
CorentinAulagnier <corentinaulagnier@hotmail.fr>
2019-08-22 11:50:29 +0200

    fix test

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 #####
 # Test variables
@@ -8,32 +9,16 @@ COMMON_NAME=COMMON_NAME_DEFAULT
 
 SLEEP=2
 
-#####
-# Removing secrets, certificates and issuers
-#####
-kubectl delete secret ca-keys-secret --namespace=${TEST_NAMESPACE} 			> /dev/null
-kubectl delete secret ca-keys-secret --namespace=cert-manager 				> /dev/null
+deleteRessources() 
 
-kubectl delete issuer issuer-selfsigned --namespace=${TEST_NAMESPACE} 			> /dev/null
-kubectl delete issuer issuer-ca --namespace=${TEST_NAMESPACE} 				> /dev/null
-kubectl delete clusterissuer clusterissuer-ca
-
-kubectl delete certificate certificate-selfsigned --namespace=${TEST_NAMESPACE} 	> /dev/null
-kubectl delete certificate certificate-ca --namespace=${TEST_NAMESPACE} 		> /dev/null
-kubectl delete certificate certificate-cluster-ca --namespace=${TEST_NAMESPACE} 	> /dev/null
-
-kubectl delete secret certificate-selfsigned-secret --namespace=${TEST_NAMESPACE} 	> /dev/null
-kubectl delete secret certificate-ca-secret --namespace=${TEST_NAMESPACE} 		> /dev/null
-kubectl delete secret certificate-cluster-ca-secret --namespace=${TEST_NAMESPACE} 	> /dev/null
-
-
-echo
-echo Self signed issuer test
-echo
 
 #####
 # Self signed issuer test
 #####
+
+echo ###############
+echo Self signed issuer test
+echo
 
 # Create a self signed issuer
 kubectl create -f issuer-selfsigned.yaml --namespace=${TEST_NAMESPACE}
@@ -49,13 +34,14 @@ sleep ${SLEEP}
 
 kubectl get secret certificate-selfsigned-secret --namespace=${TEST_NAMESPACE}
 
-echo
-echo Classic issuer test
-echo
 
 #####
 # Classic issuer test
 #####
+
+echo ###############
+echo Classic issuer test
+echo
 
 # Generate a CA private key
 openssl genrsa -out ca.key 2048
@@ -80,13 +66,14 @@ sleep ${SLEEP}
 
 kubectl get secret certificate-ca-secret --namespace=${TEST_NAMESPACE}
 
-echo
-echo Cluster issuer test
-echo
 
 #####
 # Cluster issuer test
 #####
+
+echo ###############
+echo Cluster issuer test
+echo
 
 # Generate a cluster CA private key
 openssl genrsa -out ca.key 2048
@@ -96,7 +83,7 @@ openssl req -x509 -new -nodes -key ca.key -subj "/CN=${COMMON_NAME}" -days 3650 
 
 # Create a secret with the self signed Certificate, and the CA private key
 # The secret must be in the cert-manager namespace
-kubectl create secret tls ca-keys-secret --cert=ca.crt --key=ca.key --namespace=cert-manager
+kubectl create secret tls cluster-ca-keys-secret --cert=ca.crt --key=ca.key --namespace=cert-manager
 
 # Create a cluster issuer using the previous secret
 kubectl create -f clusterIssuer-ca.yaml
@@ -116,10 +103,29 @@ echo
 echo End test
 echo
 
-# Removing CA key and cert file
-rm ca.key ca.crt
+deleteRessources() 
 
+#####
+# Removing CA key and cert file, secrets, certificates and issuers
+#####
+deleteRessources() 
+{	rm ca.key ca.crt									> /dev/null
 
+	kubectl delete secret ca-keys-secret --namespace=${TEST_NAMESPACE} 			> /dev/null
+	kubectl delete secret cluster-ca-keys-secret --namespace=cert-manager 			> /dev/null
+
+	kubectl delete issuer issuer-selfsigned --namespace=${TEST_NAMESPACE} 			> /dev/null
+	kubectl delete issuer issuer-ca --namespace=${TEST_NAMESPACE} 				> /dev/null
+	kubectl delete clusterissuer clusterissuer-ca						> /dev/null
+
+	kubectl delete certificate certificate-selfsigned --namespace=${TEST_NAMESPACE} 	> /dev/null
+	kubectl delete certificate certificate-ca --namespace=${TEST_NAMESPACE} 		> /dev/null
+	kubectl delete certificate certificate-cluster-ca --namespace=${TEST_NAMESPACE}		> /dev/null
+
+	kubectl delete secret certificate-selfsigned-secret --namespace=${TEST_NAMESPACE} 	> /dev/null
+	kubectl delete secret certificate-ca-secret --namespace=${TEST_NAMESPACE}		> /dev/null
+	kubectl delete secret certificate-cluster-ca-secret --namespace=${TEST_NAMESPACE}	> /dev/null
+}
 
 
 
