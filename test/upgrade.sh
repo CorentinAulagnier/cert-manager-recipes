2b6fcdb7933f56997786227ba84422bedb228c4c
CorentinAulagnier <corentinaulagnier@hotmail.fr>
2019-08-26 15:47:45 +0200

    update script

--- test/upgrade.sh
+++ test/upgrade.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
+#####
+# delete CRDs
+#####
+
 kubectl get crds | grep certmanager | awk '{print $1}' | xargs -I '{}' kubectl delete crd '{}'
+
+#####
+# delete cert-manager
+#####
 
 helm delete --purge cert-manager
 
-sleep 10
+#####
+# install CRDs
+#####
 
 kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.10/deploy/manifests/00-crds.yaml
 
-sleep 2
+#####
+# install cert-manager v10
+#####
 
 helm install --name cert-manager --namespace cert-manager --version v0.10.0-alpha.0  jetstack/cert-manager --set webhook.enabled=false
 
-sleep 10
+#####
+# start tests
+#####
 
 sh ./test.sh
 
